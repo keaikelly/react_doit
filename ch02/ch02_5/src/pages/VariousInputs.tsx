2ced5c68317a75c346fad5e0e37bb176c97c033f
keaikelly <keaikelly@naver.com>
2025-01-22 20:28:17 +0900

    2-5: 이벤트 속성

---
+++ ch02/ch02_5/src/pages/VariousInputs.tsx
@@ -0,0 +1,17 @@
+export default function VariousInputs() {
+  return (
+    <div>
+      <p>VariousInputs</p>
+      <div>
+        <input type="type" placeholder="enter some texts" />
+        <input type="password" placeholder="enter your password" />
+        <input type="email" placeholder="enter your email" />
+        <input type="range" />
+        <input type="button" value="I'm a button" />
+        <input type="checkbox" value="I'm a checkbox" defaultChecked />
+        <input type="radio" value="I'm a radio" defaultChecked />
+        <input type="file" />
+      </div>
+    </div>
+  )
+}
